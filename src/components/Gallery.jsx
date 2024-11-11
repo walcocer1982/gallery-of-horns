1f4e663ca31a53753279524425f2b78b8340ba45
Walther Enriquer Alcocer Gutierrez <walcocer.1982@gmail.com>
2024-11-10 20:59:36 -0500

    últimos cambio json

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,29 +1,26 @@
 import HornedBeast from './HornedBeast';
 import data from "../data.json";
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 function Gallery() {
     return (
-        <div>
-            <HornedBeast
-                title="Chivito"
-                imageUrl="https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg"
-                description="meee" />
-            <HornedBeast
-                title="Vaquita"
-                imageUrl="https://santuariovegan.org/wp-content/webp-express/webp-images/uploads/2020/06/apadrina-vaca-ezequiel-02.jpg.webp"
-                description="muuu" />
-            {
-                data.map(beast => (
-                    <HornedBeast
-                        key={beast._id}
-                        title={beast.title}
-                        imageUrl={beast.image_url}
-                        description={beast.description}
-                    />
-                ))
-            }
-        </div>
-    );
+
+        <Container>
+            <Row xs={2} sm={3} md={4} lg={5} >
+                {
+                    data.map(beast => (
+                        <HornedBeast
+                            key={beast._id}
+                            title={beast.title}
+                            imageUrl={beast.image_url}
+                            description={beast.description}
+                        />
+                    ))
+                }
+            </Row>
+        </Container>
+    )
 }
 
 export default Gallery;
