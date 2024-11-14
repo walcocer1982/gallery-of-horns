50211962af486bdc97c0fd8f7f0b1d27f9171665
Walther Enriquer Alcocer Gutierrez <walcocer.1982@gmail.com>
2024-11-13 19:37:59 -0500

    Descripción de los cambios realizados

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,20 @@
 function Header() {
-    return <h1>Bienvenidos a la galeria Horned Beasts</h1>;
+
+  function idioma(){
+    alert ("idioma")
+  } 
+const lenguaje=() => {
+alert("lenguaje")
+}
+
+    return (
+      <>
+      <h1>Bienvenidos a la galeria Horned Beasts</h1>
+      <button onClick={idioma}>Cambiar idioma</button>
+      <button onClick={lenguaje}>Cambiar lenguaje</button>
+      </>
+    );
+
   }
   
   export default Header;
